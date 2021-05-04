e4660ec40cf08c5807c84fa22a1ce0dbad0995ab
monocsp <monocsp@naver.com>
2021-05-04 21:16:31 +0900

    Update 2020_09_08_1.py

--- Python/2020_09_08_1.py
+++ Python/2020_09_08_1.py
@@ -1,24 +1,20 @@
 import sys
 
-def Drawstar(depth):
-    count = depth
-    space = 0
-    if count > 0 :
-        for i in space:
-            print(' ')
-        for i in count:
-            print('*')
-        print('\n')
-        space += 1
-        count -= 1
-    else :
-        for i in space:
-            print(' ')
-        for i in count:
-            print('*')
-
 
 
 if __name__ == '__main__':
-    count = int(sys.stdin.readline())
-    print(count)
+    numbers = list(map(int,input("hi").split()))
+
+    i, temp = 0,0
+    for i in range(len(numbers)):
+        j = i+1
+        for j in range(len(numbers)):
+            if(numbers[i]< numbers[j]):
+                temp = numbers[i]
+                numbers[i] = numbers[j]
+                numbers[j] = temp
+                print("2 i is " + str(i))
+                print("2 j is " +str(j))
+                print(numbers)
+    for i in range(len(numbers)):
+        print(numbers[i], end = " ")
