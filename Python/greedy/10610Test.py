103c39d1fa5d57c206c12c88d0d4589f11fe042a
monocsp <monocsp@naver.com>
2022-01-14 00:19:49 +0900

    20220114 baekjoon
    10610 Test success

--- Python/greedy/10610Test.py
+++ Python/greedy/10610Test.py
@@ -1,6 +1,14 @@
-receive_number_list = map(int,input())
+receive_number_list = list(map(int, input()))
 if 0 in receive_number_list:
     receive_number_list.remove(0)
-    
+    if sum(receive_number_list) % 3 == 0:
+        receive_number_list.sort(reverse=True)
+        result = ""
+        for i in receive_number_list:
+            result += str(i)
+        print(result+"0")
+    else:
+        print(-1)
 else:
     print(-1)
+
