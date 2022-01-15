bc4fd2ec966be9090d4b3bf3796e04d27223df76
monocsp <monocsp@naver.com>
2022-01-15 17:43:56 +0900

    Update 1789Test.py

--- Python/greedy/1789Test.py
+++ Python/greedy/1789Test.py
@@ -3,10 +3,10 @@ max_number = 0
 while True:
     ad = receive_int//3
 
-    for i in range(ad,0,-1):
+    for i in range(ad, 0, -1):
         max_number = i
         sum = 0
-        for element in range(i,0,-1):
+        for element in range(i, 0, -1):
             sum += element
             if sum > receive_int:
                 max_number = 0
@@ -15,3 +15,7 @@ while True:
         if max_number != 0:
             break
 
+    if max_number != 0:
+        break
+
+print(max_number)
