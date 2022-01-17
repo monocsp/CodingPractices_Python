1d56ffa2053e8bdafc410ba345e62b441ef09183
monocsp <monocsp@naver.com>
2022-01-17 23:56:58 +0900

    Update 1946Test.py

--- Python/greedy/1946Test.py
+++ Python/greedy/1946Test.py
@@ -1,27 +1,46 @@
 receive_test_count = int(input())
 
+
+
 def hiring(count):
     grade = []
     drop_count = 0
+
+    first_document_grade = []
+    first_interview_grade = []
     #grade받아오기
     for i in range(0, count):
-        grade.append(list(map(int, input().split())))
-    #서류기준으로 탈락자
-    grade.sort(key=lambda x:x[0])
-    base_interview_grade = grade[0][1]
-    for index, i in enumerate(grade):
-        current_interview_grade = i[1]
-        if current_interview_grade > base_interview_grade:
-            drop_count += 1
+        document_grade, interview_grade = map(int, input().split())
 
-    # 면접기준으로 탈락자
-    grade.sort(key=lambda x:x[1])
-    base_document_grade = grade[0][0]
-    for index, i in enumerate(grade):
+        if document_grade == 1:
+            first_document_grade.append(document_grade)
+            first_document_grade.append( interview_grade)
+        if interview_grade == 1:
+            first_interview_grade.append(document_grade)
+            first_interview_grade.append(interview_grade)
+
+        # if len(first_document_grade) == 2:
+        #     if interview_grade > first_interview_grade[1]:
+        #         continue
+        #
+        # if len(first_interview_grade) == 2:
+        #     if document_grade > first_document_grade[0]:
+        #         continue
+
+        grade.append([document_grade, interview_grade])
+
+    for i in grade:
         current_document_grade = i[0]
-        if current_document_grade > base_document_grade:
-            drop_count += 1
+        current_interview_grade = i[1]
 
+        #서류기준으로 탈락자
+        #면접순위로 비교
+        if current_interview_grade > first_document_grade[1]:
+            drop_count += 1
+        # 면접기준으로 탈락자
+        # 서류순위로 비교
+        elif current_document_grade > first_interview_grade[0]:
+            drop_count += 1
 
 
     return (count - drop_count)
