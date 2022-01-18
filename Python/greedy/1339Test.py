ac53d92e308b1d5bf345fbf2481ce0fc64111fe0
monocsp <monocsp@naver.com>
2022-01-19 00:45:52 +0900

    Update 1339Test.py

--- Python/greedy/1339Test.py
+++ Python/greedy/1339Test.py
@@ -2,7 +2,13 @@ word_count = int(input())
 #2차원 배열
 #len - index = 자릿수
 word_list = []
+total_result = 0
+word_dic = {1:[],2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[]}
 for i in range(0, word_count):
-    word_list.append(input().split())
+    receive_word = [char for char in input()]
 
-print(word_list)
+    word_list.append([char for char in input()])
+
+
+
+
