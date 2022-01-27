c0c7f73eb57b242118208b9ca789e6a45a7f9eac
Chan Seob Park <55018831+monocsp@users.noreply.github.com>
2022-01-27 23:24:20 +0900

    Update 7568Test.py

--- Python/7568Test.py
+++ Python/7568Test.py
@@ -4,4 +4,5 @@ for i in range(0, receive_count):
     weight, height = map(int, input().split())
     size_list.append([weight, height])
 
-sort_height
+sort_weight = size_list.sort(key = lambda x:(x[0],-x[1]))
+sort_height = size_list.sort(key = lambda x:x[0])
