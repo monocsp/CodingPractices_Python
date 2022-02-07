79a42fc800022a2e752a592367738d68ded992c4
monocsp <monocsp@naver.com>
2022-02-08 01:16:02 +0900

    Update 2812Test.py

--- Python/greedy/2812Test.py
+++ Python/greedy/2812Test.py
@@ -1,7 +1,10 @@
 digit_count, dismiss_count = map(int,input().split())
 r_digit = list(input())
 
+#가장 큰 값을 잘라서 앞에서부터 작은 숫자순서대로 자른다.
+max_number = 0
 for i in range(0, dismiss_count):
-    
+    max_number_index =
 
 print(''.join(r_digit))
+
