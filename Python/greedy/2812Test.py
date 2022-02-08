5c044f6d4d76a757c2b171011b952718ffb05c83
monocsp <monocsp@naver.com>
2022-02-08 23:40:38 +0900

    Update 2812Test.py

--- Python/greedy/2812Test.py
+++ Python/greedy/2812Test.py
@@ -1,10 +1,29 @@
 digit_count, dismiss_count = map(int,input().split())
 r_digit = list(input())
 
-#가장 큰 값을 잘라서 앞에서부터 작은 숫자순서대로 자른다.
-max_number = 0
-for i in range(0, dismiss_count):
-    max_number_index =
+#Priority list를 만든다
+#만드는방법 list[n] < list[n+1]일때 생성
+start_index = 0
+priority_list = []
+while True:
+    i = 0
+    if r_digit[i] < r_digit[i+1]:
+        temp_list = r_digit[start_index:i+1]
+        temp_list.sort()
+        start_index = i+1
+        priority_list.append( temp_list)
+
+    if i+1 == len(r_digit) -1:
+        break
+
+    if len(priority_list) == dismiss_count:
+        break
+
+if len(priority_list) == 0:
+    for i in range(0, dismiss_count):
+        r_digit.remove(min(rdigit))
+else:
+    for i in priority_list:
+        r_digit.remove(i)
 
 print(''.join(r_digit))
-
