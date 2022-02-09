15264a1c985893d74ab5c12922294972769d1954
monocsp <monocsp@naver.com>
2022-02-09 23:31:37 +0900

    Update 2812Test.py

--- Python/greedy/2812Test.py
+++ Python/greedy/2812Test.py
@@ -5,19 +5,29 @@ r_digit = list(input())
 #만드는방법 list[n] < list[n+1]일때 생성
 start_index = 0
 priority_list = []
+i = 0
 while True:
-    i = 0
-    if r_digit[i] < r_digit[i+1]:
-        temp_list = r_digit[start_index:i+1]
-        temp_list.sort()
-        start_index = i+1
-        priority_list.append( temp_list)
-
-    if i+1 == len(r_digit) -1:
-        break
 
     if len(priority_list) == dismiss_count:
         break
+
+    print(f"current start index : {start_index}")
+    if r_digit[i] < r_digit[i+1]:
+        temp_list = r_digit[start_index:i+1]
+        temp_list.sort()
+        start_index = i+2
+        priority_list += temp_list
+        print(f"current temp_list : {temp_list}")
+
+    if i+1 == len(r_digit) -1:
+        temp_list = r_digit[start_index:i + 1]
+        temp_list.sort()
+        priority_list += temp_list
+
+    i += 1
+
+print(r_digit)
+print(priority_list)
 
 if len(priority_list) == 0:
     for i in range(0, dismiss_count):
